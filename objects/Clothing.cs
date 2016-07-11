0411d250632c19ec5f582ab57276ec099582a149
Adam Luchini and George Olson <adam+george@>
2016-07-11 14:20:55 -0700

    Add tshirt table functionality and successfully passed tests

---
+++ objects/Clothing.cs
@@ -0,0 +1,167 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace Closet
+{
+  public class Tshirt
+  {
+    private int _id;
+    private string _color;
+
+    public Tshirt(string Color, int Id = 0)
+    {
+      _id = Id;
+      _color = Color;
+    }
+
+    // public Pants(string Color, int Id = 0)
+    // {
+    //   _id = Id;
+    //   _color = Color;
+    // }
+    //
+    // public Socks(string Color, int Id = 0)
+    // {
+    //   _id = Id;
+    //   _color = Color;
+    // }
+
+    public override bool Equals(System.Object otherTshirt)
+    {
+      if (!(otherTshirt is Tshirt))
+      {
+        return false;
+      }
+      else
+      {
+        Tshirt newTshirt = (Tshirt) otherTshirt;
+        bool idEquality = (this.GetId() == newTshirt.GetId());
+        bool colorEquality = (this.GetColor() == newTshirt.GetColor());
+        return (idEquality && colorEquality);
+      }
+    }
+
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetColor()
+    {
+      return _color;
+    }
+    public void SetColor(string newColor)
+    {
+      _color = newColor;
+    }
+
+    public static List<Tshirt> GetAll()
+    {
+      List<Tshirt> allTshirts = new List<Tshirt>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT id, color FROM tshirts;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int tshirtId = rdr.GetInt32(0);
+        string tshirtColor = rdr.GetString(1);
+        Tshirt newTshirt = new Tshirt(tshirtColor, tshirtId);
+        allTshirts.Add(newTshirt);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allTshirts;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO tshirts (color) OUTPUT INSERTED.id VALUES (@TshirtColor);", conn);
+
+      SqlParameter colorParameter = new SqlParameter();
+      colorParameter.ParameterName = "@TshirtColor";
+      colorParameter.Value = this.GetColor();
+      cmd.Parameters.Add(colorParameter);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM tshirts;", conn);
+      cmd.ExecuteNonQuery();
+    }
+
+    public static Tshirt Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT id, color FROM tshirts WHERE id = @TshirtId;", conn);
+      SqlParameter tshirtIdParameter = new SqlParameter();
+      tshirtIdParameter.ParameterName = "@TshirtId";
+      tshirtIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(tshirtIdParameter);
+      rdr = cmd.ExecuteReader();
+
+      int foundTshirtId = 0;
+      string foundTshirtColor = null;
+      while(rdr.Read())
+      {
+        foundTshirtId = rdr.GetInt32(0);
+        foundTshirtColor = rdr.GetString(1);
+      }
+      Tshirt foundTshirt = new Tshirt(foundTshirtColor, foundTshirtId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return foundTshirt;
+    }
+
+    public override int GetHashCode()
+    {
+         return this.GetColor().GetHashCode();
+    }
+
+
+  }
+}
---
+++ tests/Clothing_test.cs
@@ -0,0 +1,89 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Closet
+{
+  public class TshirtTest : IDisposable
+  {
+    public TshirtTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=closet_database;Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+      Tshirt.DeleteAll();
+    }
+
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Tshirt.GetAll().Count;
+
+      //Assert
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_Equal_ReturnsTrueIfColorsAreTheSame()
+    {
+      //Arrange, Act
+      Tshirt firstTshirt = new Tshirt("red");
+      Tshirt secondTshirt = new Tshirt("red");
+
+      //Assert
+      Assert.Equal(firstTshirt, secondTshirt);
+    }
+
+    [Fact]
+    public void Test_Save_SavesToDatabase()
+    {
+      //Arrange
+      Tshirt testTshirt = new Tshirt("green");
+
+      //Act
+      testTshirt.Save();
+      List<Tshirt> result = Tshirt.GetAll();
+      List<Tshirt> testList = new List<Tshirt>{testTshirt};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_Save_AssignIdToObject()
+    {
+
+      //Arrange
+      Tshirt testTshirt = new Tshirt("blue");
+
+      //Act
+      testTshirt.Save();
+      Tshirt savedTshirt = Tshirt.GetAll()[0];
+
+      int result = savedTshirt.GetId();
+      int testId = testTshirt.GetId();
+
+      //Assert
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void Test_Find_FindsTshirtInDatabase()
+    {
+      //Arrange
+      Tshirt testTshirt = new Tshirt("green");
+      testTshirt.Save();
+
+      //Act
+      Tshirt foundTshirt = Tshirt.Find(testTshirt.GetId());
+
+      //Assert
+      Assert.Equal(testTshirt, foundTshirt);
+    }
+  }
+}
